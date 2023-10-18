cd0156409e51ed631b2408dbaeb8cd680b671edb
Gunjan <realgunjanbhasin@gmail.com>
2023-10-18 23:45:32 +0530

    add add.cpp

---
+++ add.cpp
@@ -0,0 +1,14 @@
+/*2:Write a program to adddition of two numbers .*/
+#include<iostream>
+using namespace std;
+int main (){
+	int num1,num2;
+	int w;
+	cout<<"enter num 1"<<endl;
+	cin>>num1;
+	cout<<"enter num 2"<<endl;
+	cin>>num2;
+	w=num1+num2;
+	cout<<"addition of two numbers is "<<w<<endl;
+	return 0;
+}
